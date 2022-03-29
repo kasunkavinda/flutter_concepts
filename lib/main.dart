3b3dbee95377405742ac5833d0644d593f33f506
KasunKavindaGeveo <kasun.k@geveo.com>
2022-03-29 16:32:58 +0530

    profile incrementer

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,18 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MaterialApp(
-  home: Home()
+    home: Home()
 ));
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int ninjaLevel = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,43 +29,51 @@ class Home extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(30.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Center(
-              child: CircleAvatar(
-                backgroundImage: NetworkImage("https://avatars.githubusercontent.com/u/26274844?v=4"),
-                radius: 100,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Center(
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage("https://avatars.githubusercontent.com/u/26274844?v=4"),
+                  radius: 100,
+                ),
               ),
-            ),
-            const SizedBox(height: 30),
-            Divider( height: 60.0, color: Colors.grey[600]),
-            const Text('NAME', style: TextStyle( color: Colors.white, letterSpacing: 2)),
-            const SizedBox(height: 5),
-            const Text('Kasun Kavinda', style: TextStyle( color: Colors.amber, letterSpacing: 2, fontSize: 20, fontWeight: FontWeight.bold)),
-            const SizedBox(height: 20),
-            const Text('CURRENT POSITION', style: TextStyle( color: Colors.white, letterSpacing: 2)),
-            const SizedBox(height: 5),
-            const Text('Software Engineer', style: TextStyle( color: Colors.amber, letterSpacing: 2, fontSize: 20, fontWeight: FontWeight.bold)),
-            const SizedBox(height: 20),
-            const Text('COMPANY', style: TextStyle( color: Colors.white, letterSpacing: 2)),
-            const SizedBox(height: 5),
-            const Text('Geveo Australasia', style: TextStyle( color: Colors.amber, letterSpacing: 2, fontSize: 20, fontWeight: FontWeight.bold)),
-            const SizedBox(height: 20),
-            Row(
-              children: const [
-               Icon(
-                 Icons.email,
-                 color: Colors.white
-               ),
-               SizedBox(width: 5),
-               Text('kasunkavinda.bc@gmail.com', style: TextStyle(color: Colors.white, fontSize: 15, letterSpacing: 2))
-              ],
-            ),
-          ]
+              const SizedBox(height: 30),
+              Divider( height: 60.0, color: Colors.grey[600]),
+              const Text('NAME', style: TextStyle( color: Colors.white, letterSpacing: 2)),
+              const SizedBox(height: 5),
+              const Text('Kasun Kavinda', style: TextStyle( color: Colors.amber, letterSpacing: 2, fontSize: 20, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 20),
+              const Text('CURRENT POSITION', style: TextStyle( color: Colors.white, letterSpacing: 2)),
+              const SizedBox(height: 5),
+              const Text('Software Engineer', style: TextStyle( color: Colors.amber, letterSpacing: 2, fontSize: 20, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 20),
+              //const Text('COMPANY', style: TextStyle( color: Colors.white, letterSpacing: 2)),
+              //const SizedBox(height: 5),
+              //const Text('Geveo Australasia', style: TextStyle( color: Colors.amber, letterSpacing: 2, fontSize: 20, fontWeight: FontWeight.bold)),
+              //const SizedBox(height: 20),
+              const Text('LEVEL', style: TextStyle( color: Colors.white, letterSpacing: 2)),
+              const SizedBox(height: 5),
+              Text('$ninjaLevel', style: TextStyle( color: Colors.amber, letterSpacing: 2, fontSize: 20, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 20),
+              Row(
+                children: const [
+                  Icon(
+                      Icons.email,
+                      color: Colors.white
+                  ),
+                  SizedBox(width: 5),
+                  Text('kasunkavinda.bc@gmail.com', style: TextStyle(color: Colors.white, fontSize: 15, letterSpacing: 2))
+                ],
+              ),
+            ]
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: (){
+          setState(() {
+            ninjaLevel = ninjaLevel + 1;
+          });
+        },
         child: const Text('Add'),
       ),
     );
