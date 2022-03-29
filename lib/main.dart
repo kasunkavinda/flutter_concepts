541c876f23c0d440207991b97ad7c4779abfc714
KasunKavindaGeveo <kasun.k@geveo.com>
2022-03-29 10:48:03 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('My First App'),
-      centerTitle: true,
-    ),
-    body:Center(
-      child: Text('kasun kavinda')
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: (){},
-      child: Text('Add'),
-    ),
-  )
+  home: Home()
 ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'My First Appp',
+          style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2,
+              color: Colors.amber
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple[900],
+      ),
+      body:Center(
+          child: Image(
+            image: AssetImage('assets/image1.jpg')
+          )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Text('Add'),
+      ),
+    );
+  }
+}
+
 
