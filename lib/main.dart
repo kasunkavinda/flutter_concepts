5658f3b52ba4b19af37d9dbc314ffc3cd4c2d78e
KasunKavindaGeveo <kasun.k@geveo.com>
2022-03-29 15:24:40 +0530

    profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MaterialApp(
@@ -10,55 +11,52 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black38,
       appBar: AppBar(
         title: const Text(
-          'My First App',
-          style: TextStyle(
-              fontSize: 30.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2,
-              color: Colors.amber
-          ),
+          'Profile ID Card',
         ),
         centerTitle: true,
         backgroundColor: Colors.deepPurple[900],
+        elevation: 0,
       ),
-      body: Row(
-        children: [
-          Expanded(
-            child: Image.asset('assets/image1.jpg'),
-            flex: 4),
-          Expanded(
-            flex: 3,
-            child: Container(
-              color: Colors.cyan,
-              child: const Padding(
-                padding: EdgeInsets.all(50.0),
-                child: Text('1'),
-              )
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Center(
+              child: CircleAvatar(
+                backgroundImage: NetworkImage("https://avatars.githubusercontent.com/u/26274844?v=4"),
+                radius: 100,
+              ),
             ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-                color: Colors.pink,
-                child: const Padding(
-                  padding: EdgeInsets.all(50.0),
-                  child: Text('1'),
-                )
+            const SizedBox(height: 30),
+            Divider( height: 60.0, color: Colors.grey[600]),
+            const Text('NAME', style: TextStyle( color: Colors.white, letterSpacing: 2)),
+            const SizedBox(height: 5),
+            const Text('Kasun Kavinda', style: TextStyle( color: Colors.amber, letterSpacing: 2, fontSize: 20, fontWeight: FontWeight.bold)),
+            const SizedBox(height: 20),
+            const Text('CURRENT POSITION', style: TextStyle( color: Colors.white, letterSpacing: 2)),
+            const SizedBox(height: 5),
+            const Text('Software Engineer', style: TextStyle( color: Colors.amber, letterSpacing: 2, fontSize: 20, fontWeight: FontWeight.bold)),
+            const SizedBox(height: 20),
+            const Text('COMPANY', style: TextStyle( color: Colors.white, letterSpacing: 2)),
+            const SizedBox(height: 5),
+            const Text('Geveo Australasia', style: TextStyle( color: Colors.amber, letterSpacing: 2, fontSize: 20, fontWeight: FontWeight.bold)),
+            const SizedBox(height: 20),
+            Row(
+              children: const [
+               Icon(
+                 Icons.email,
+                 color: Colors.white
+               ),
+               SizedBox(width: 5),
+               Text('kasunkavinda.bc@gmail.com', style: TextStyle(color: Colors.white, fontSize: 15, letterSpacing: 2))
+              ],
             ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-                color: Colors.amber,
-                child: const Padding(
-                  padding: EdgeInsets.all(50.0),
-                  child: Text('1'),
-                )
-            ),
-          ),
-        ],
+          ]
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){},
